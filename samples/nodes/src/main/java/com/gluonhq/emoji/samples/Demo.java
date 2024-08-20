dd3e9e95d25e3bfd204473741e94767d1e292aae
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2024-08-20 18:08:21 +0530

    update demo as per feedback

--- samples/nodes/src/main/java/com/gluonhq/emoji/samples/Demo.java
+++ samples/nodes/src/main/java/com/gluonhq/emoji/samples/Demo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, Gluon
+ * Copyright (c) 2023, 2024, Gluon
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -49,38 +49,56 @@ import java.util.stream.Collectors;
 
 public class Demo extends Application {
 
+    private StackPane root;
+
     @Override
     public void start(Stage primaryStage) {
 
-        ProgressIndicator progressIndicator = new ProgressIndicator();
-        Scene scene = new Scene(new StackPane(progressIndicator), 1230, 800);
-        TextFlow textFlow = new TextFlow();
-        textFlow.prefWidthProperty().bind(scene.widthProperty().subtract(30));
+        root = new StackPane();
+        Scene scene = new Scene(root, 1230, 800);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Emoji nodes: " +
-                textFlow.getChildren().stream().filter(ImageView.class::isInstance).count());
         primaryStage.show();
 
-        EmojiLoaderFactory.getEmojiImageLoader().initialize().thenAccept(aBoolean -> {
-            if (aBoolean) {
-                // Get a single text string with all unicode characters for all emojis
-                String text = getAllEmojisAsUnicodeString();
-                // Convert text to Text (if delimiter is not empty) and Image nodes (all emojis)
-                List<Node> emojiNodes = TextUtils.convertToTextAndImageNodes(text);
-                emojiNodes.stream()
-                        .filter(ImageView.class::isInstance)
-                        .forEach(node -> {
-                            String unified = (String) node.getProperties().get(EmojiImageUtils.IMAGE_VIEW_EMOJI_PROPERTY);
-                            EmojiData.emojiFromCodepoints(unified).ifPresent(emoji ->
-                                    Tooltip.install(node, new Tooltip("Emoji: " + emoji.getName() + "\n" + unified)));
-                        });
-                // Add nodes to textFlow
-                Platform.runLater(() -> {
-                    textFlow.getChildren().addAll(emojiNodes);
-                    scene.setRoot(new ScrollPane(textFlow));
-                });
-            }
+        if (EmojiLoaderFactory.getEmojiImageLoader().isInitialized()) {
+            addEmojis();
+        } else {
+            ProgressIndicator progressIndicator = new ProgressIndicator();
+            root.getChildren().setAll(progressIndicator);
+            EmojiLoaderFactory.getEmojiImageLoader().initialize().thenAccept(aBoolean -> {
+                if (aBoolean) {
+                    addEmojis();
+                }
+            });
+        }
+    }
+
+    private void addEmojis() {
+        TextFlow textFlow = new TextFlow();
+        Scene scene = root.getScene();
+        textFlow.prefWidthProperty().bind(scene.widthProperty().subtract(30));
+        List<Node> emojiNodes = processEmojis();
+        // Add nodes to textFlow
+        Platform.runLater(() -> {
+            textFlow.getChildren().addAll(emojiNodes);
+            root.getChildren().setAll(new ScrollPane(textFlow));
+            ((Stage)scene.getWindow()).setTitle("Emoji nodes: " +
+                    textFlow.getChildren().stream().filter(ImageView.class::isInstance).count());
         });
+    }
+
+    private List<Node> processEmojis() {
+        // Get a single text string with all unicode characters for all emojis
+        String text = getAllEmojisAsUnicodeString();
+        // Convert text to Text (if delimiter is not empty) and Image nodes (all emojis)
+        List<Node> emojiNodes = TextUtils.convertToTextAndImageNodes(text);
+        emojiNodes.stream()
+                .filter(ImageView.class::isInstance)
+                .forEach(node -> {
+                    String unified = (String) node.getProperties().get(EmojiImageUtils.IMAGE_VIEW_EMOJI_PROPERTY);
+                    EmojiData.emojiFromCodepoints(unified).ifPresent(emoji ->
+                            Tooltip.install(node, new Tooltip("Emoji: " + emoji.getName() + "\n" + unified)));
+                });
+        return emojiNodes;
     }
 
     /**
